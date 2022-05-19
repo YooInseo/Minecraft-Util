e4b10da853844df5d778d038ee1e2954cb726654
Inseo <48828697+YooInseo@users.noreply.github.com>
2022-05-19 12:19:47 +0900

    Add files via upload

---
+++ js/inventory.js
@@ -0,0 +1,12 @@
+var colum;
+
+var size;
+var name;
+
+function Create(){
+
+    colum = $('#colum').val()
+    for(int = 0; int < size; int++){
+        
+    }
+}
---
+++ js/itemstack.js
@@ -0,0 +1,10 @@
+var name;
+var type;
+var loc;
+
+function findimage(){
+    var image = <img src={require('../src/items/'+ type + '.png')} />
+
+
+    return image;
+}
