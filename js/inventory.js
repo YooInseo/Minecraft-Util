5f2ed086c33bfcd29c593206f0ab8bc23af1b504
Inseo <48828697+YooInseo@users.noreply.github.com>
2022-05-20 21:05:53 +0900

    Add files via upload

--- js/inventory.js
+++ js/inventory.js
@@ -2,6 +2,8 @@ var colum;
 
 var size;
 var name;
+var sizetype = sizetype
+var obj = document.getElementById("inventory");
 
 function Create(){
 
@@ -9,4 +11,13 @@ function Create(){
     for(int = 0; int < size; int++){
         
     }
+
+}
+const sizetype = {
+    9: obj.style.width = 500,
+    18: 18,
+    27: 27,
+    36: 36,
+    45: 45,
+    54:54
 }
